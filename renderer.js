338d447987446791dd536b7a214f5d9edae65b96
salamander-r <pyvovar.roman9754@gmail.com>
2024-08-25 13:35:10 +0300

    added changes with graphs sin x

--- renderer.js
+++ renderer.js
@@ -1,10 +1,23 @@
-alert("Test");
 const ctx = document.getElementById('myChart');
-        new Chart(ctx, {
-            type: 'line',
-            data: {
-                datasets: [{
-                    data: [{ x: 1, y: 1 }, { x: 3, y: 3 }]
-                }]
-            }
-        });
+const button = document.getElementById('submit');
+let sinChart = null;
+button.onclick = () => {
+    if (sinChart !== null) {
+        sinChart.destroy();
+    }
+    const x0 = parseInt(document.getElementById('x0').value);
+    const xn = parseInt(document.getElementById('xn').value);
+    const points = [];
+    for (let x = x0; x < xn; x += 0.25) {
+        points.push({ x: `${x}`, y: `${Math.sin(x)}` });
+    }
+
+    sinChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                data: points
+            }]
+        }
+    });
+};
