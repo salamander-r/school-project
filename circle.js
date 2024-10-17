6c5f3220ba3d54f4b06f86e18ec969ccc2fa63a9
salamander-r <pyvovar.roman9754@gmail.com>
2024-10-17 20:50:06 +0300

    added projections of sin/cos in circle

--- circle.js
+++ circle.js
@@ -3,7 +3,9 @@
     const button = document.getElementById('submitCircle');
     let circleChart = null;
     const round = (x) => Math.round(x * 10000) / 10000;
+    const round2 = (x) => Math.round(x * 100) / 100;
     const handleClick = () => {
+        const angleRadians = Math.PI/4;
         const points = [];
         for (let x = -1; x <= 1; x += 0.01) {
             points.push({ x: `${round(x)}`, y: `${round(Math.sqrt(1 - x * x))}` });
@@ -13,6 +15,7 @@
             points.push({ x: `${x}`, y: `${-1 * round(Math.sqrt(1 - x * x))}` });
         }
         console.log(points);
+        console.log("Math.cos(angleRadians)", Math.cos(angleRadians));
         circleChart = new Chart(ctx, {
             type: 'line',
             data: {
@@ -29,6 +32,13 @@
                 {
                     data: [{ x: "0", y: "-1"}, { x: "0", y: "1"}],
                     label: 'Y',
+                    pointRadius: 0
+                },
+                {
+                    data: [{ x: "0", y: `${round2(Math.sin(angleRadians))}`}, 
+                           { x: `${round2(Math.cos(angleRadians))}`, y: `${round2(Math.sin(angleRadians))}`}, 
+                           { x: `${round2(Math.cos(angleRadians))}`, y: "0"}],
+                    label: 'Проекції',
                     pointRadius: 0
                 }]
             },
