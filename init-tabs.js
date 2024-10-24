54887fed9468ce83462958b2184a51eb426cc026
salamander-r <pyvovar.roman9754@gmail.com>
2024-10-24 20:29:16 +0300

    added new tab tests

--- init-tabs.js
+++ init-tabs.js
@@ -1,18 +1,29 @@
 (function () {
     const chartsSection = document.getElementById('charts');
     const circleSection = document.getElementById('circle');
+    const testsSection = document.getElementById('tests');
     const showChartsButton = document.getElementById('showCharts');
     const showCircleButton = document.getElementById('showCircle');
+    const showTestsButton = document.getElementById('showTests')
 
     circleSection.style.display = 'none';
+    testsSection.style.display = 'none';
 
     showChartsButton.onclick = () => {
         chartsSection.style.display = 'block';
         circleSection.style.display = 'none';
+        testsSection.style.display = 'none';
     }
 
     showCircleButton.onclick = () => {
         chartsSection.style.display = 'none';
         circleSection.style.display = 'block';
+        testsSection.style.display = 'none';
+    }
+
+    showTestsButton.onclick = () => {
+        chartsSection.style.display = 'none';
+        circleSection.style.display = 'none';
+        testsSection.style.display = 'block';
     }
 })()
