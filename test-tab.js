472283896f039ff78f84c8a2fecdfa4530aa8e30
salamander-r <pyvovar.roman9754@gmail.com>
2024-11-14 19:57:55 +0200

    added randomizer for array-tests

--- test-tab.js
+++ test-tab.js
@@ -5,6 +5,13 @@
         const questionStatus = document.getElementById("question-status");
         const correct = '✅';
         const incorrect = '❌';
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i >= 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+        }
+        shuffleArray(tests);
 
         function clearDiv() {
             testQuestionSection.innerHTML = '';
