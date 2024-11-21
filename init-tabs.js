539ff78e57004ed6d9f58e50ade6147c67b79433
salamander-r <pyvovar.roman9754@gmail.com>
2024-11-21 19:21:36 +0200

    added theory-tab

--- init-tabs.js
+++ init-tabs.js
@@ -1,27 +1,41 @@
 (function () {
+    const theorySection = document.getElementById('theory');
     const chartsSection = document.getElementById('charts');
     const circleSection = document.getElementById('circle');
     const testsSection = document.getElementById('tests');
+
+    const showTheoryButton = document.getElementById('showTheory');
     const showChartsButton = document.getElementById('showCharts');
     const showCircleButton = document.getElementById('showCircle');
-    const showTestsButton = document.getElementById('showTests')
+    const showTestsButton = document.getElementById('showTests');
 
+    chartsSection.style.display = 'none';
     circleSection.style.display = 'none';
     testsSection.style.display = 'none';
 
+    showTheoryButton.onclick = () => {
+        theorySection.style.display = 'block';
+        chartsSection.style.display = 'none';
+        circleSection.style.display = 'none';
+        testsSection.style.display = 'none';
+    }
+
     showChartsButton.onclick = () => {
+        theorySection.style.display = 'none';
         chartsSection.style.display = 'block';
         circleSection.style.display = 'none';
         testsSection.style.display = 'none';
     }
 
     showCircleButton.onclick = () => {
+        theorySection.style.display = 'none';
         chartsSection.style.display = 'none';
         circleSection.style.display = 'block';
         testsSection.style.display = 'none';
     }
 
     showTestsButton.onclick = () => {
+        theorySection.style.display = 'none';
         chartsSection.style.display = 'none';
         circleSection.style.display = 'none';
         testsSection.style.display = 'block';
