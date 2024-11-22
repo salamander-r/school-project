251c60c0f538b140e68118df5c53af95e24daa97
salamander-r <pyvovar.roman9754@gmail.com>
2024-11-22 11:44:28 +0200

    added info-tab + some info into this tab

--- init-tabs.js
+++ init-tabs.js
@@ -1,19 +1,31 @@
 (function () {
+    const infoSection = document.getElementById('info');
     const theorySection = document.getElementById('theory');
     const chartsSection = document.getElementById('charts');
     const circleSection = document.getElementById('circle');
     const testsSection = document.getElementById('tests');
 
+    const showInfoButton = document.getElementById('showInfo');
     const showTheoryButton = document.getElementById('showTheory');
     const showChartsButton = document.getElementById('showCharts');
     const showCircleButton = document.getElementById('showCircle');
     const showTestsButton = document.getElementById('showTests');
 
+    theorySection.style.display = 'none';
     chartsSection.style.display = 'none';
     circleSection.style.display = 'none';
     testsSection.style.display = 'none';
 
+    showInfoButton.onclick = () => {
+        infoSection.style.display = 'block';
+        theorySection.style.display = 'none';
+        chartsSection.style.display = 'none';
+        circleSection.style.display = 'none';
+        testsSection.style.display = 'none';
+    }
+
     showTheoryButton.onclick = () => {
+        infoSection.style.display = 'none';
         theorySection.style.display = 'block';
         chartsSection.style.display = 'none';
         circleSection.style.display = 'none';
@@ -21,6 +33,7 @@
     }
 
     showChartsButton.onclick = () => {
+        infoSection.style.display = 'none';
         theorySection.style.display = 'none';
         chartsSection.style.display = 'block';
         circleSection.style.display = 'none';
@@ -28,6 +41,7 @@
     }
 
     showCircleButton.onclick = () => {
+        infoSection.style.display = 'none';
         theorySection.style.display = 'none';
         chartsSection.style.display = 'none';
         circleSection.style.display = 'block';
@@ -35,6 +49,7 @@
     }
 
     showTestsButton.onclick = () => {
+        infoSection.style.display = 'none';
         theorySection.style.display = 'none';
         chartsSection.style.display = 'none';
         circleSection.style.display = 'none';
